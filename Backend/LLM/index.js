16fe320d0dd4dff6bdca611451cc915164ec8744
ChandraPranjal <iampranjalchandra@gmail.com>
2023-10-24 00:23:44 +0530

    algo upd

--- Backend/LLM/index.js
+++ Backend/LLM/index.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
+const axios = require('axios'); // Import Axios for making API requests
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.post('/recommendMovies', (req, res) => {
-  const movie1 = req.body.movie1;
-  const movie2 = req.body.movie2;
-  console.log(movie1 , movie2);
+  const movie1 = Number(req.body.movie1);
+  const movie2 = Number(req.body.movie2);
+  const languages = req.body.languages; // Get the list of selected languages
 
   // Replace the Python script path with your actual Python script path
   const pythonScriptPath = '/home/pc/Desktop/Begining/webdev/HackOn/Final/Backend/LLM/python_script.py';
 
-  // Construct the Python command to run the script with the user's input
-  const command = `python ${pythonScriptPath} ${movie1} ${movie2}`;
+  // Construct the Python command to run the script with the user's input and languages
+  const command = `python ${pythonScriptPath} ${movie1} ${movie2} ${languages.join(' ')}`; // Pass the languages as space-separated values
 
   exec(command, (error, stdout, stderr) => {
     if (error) {
@@ -22,12 +23,45 @@ app.post('/recommendMovies', (req, res) => {
       res.status(500).send('Internal Server Error');
       return;
     }
-
+    
     // Assuming the Python script prints the recommendations to stdout
     const recommendations = stdout.trim().split('\n');
+    
+    console.log("Recommendation received" , recommendations);
+    // Call the Rapid API for each recommended movie ID and collect the responses
+    const rapidApiOptions = {
+      method: 'GET',
+      url: 'https://mdblist.p.rapidapi.com/',
+      headers: {
+        'X-RapidAPI-Key': '7d68801094msh194b333ba34d4c7p10434cjsnce3648dceb62',
+        'X-RapidAPI-Host': 'mdblist.p.rapidapi.com'
+      }
+    };
 
-    // Send the recommendations back as a JSON response
-    res.json({ recommendations });
+    const responses = [];
+
+    // Use async/await to make the API requests and collect the responses
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+    async function fetchRecommendations() {
+      for (const movieId of recommendations) {
+        console.log("Movie id" , Number(movieId) );
+        rapidApiOptions.params = { tm: Number(movieId) };
+        try {
+          const response = await axios.request(rapidApiOptions);
+          console.log(response.data);
+          responses.push(response.data);
+        } catch (error) {
+          console.error(`Error calling Rapid API: ${error}`);
+        }
+        // Add a delay between requests (e.g., 2 seconds)
+        await delay(2000);
+      }
+      // Send the responses back as a JSON array
+      res.json(responses);
+    }
+
+    fetchRecommendations();
   });
 });
 
--- Backend/LLM/python_script.py
+++ Backend/LLM/python_script.py
@@ -14,7 +14,7 @@ def embed(texts):
     return model(texts)
 
 df = pd.read_csv("Top_10000_Movies.csv", engine="python")
-df = df[["original_title", "overview", "popularity"]]  # Include "popularity" in the data
+df = df[["id", "overview", "popularity", "original_title", "original_language"]]  # Include "original_language" in the data
 df = df.dropna()
 df = df.reset_index()
 df = df[:5500]
@@ -22,28 +22,42 @@ df = df[:5500]
 titles = list(df['overview'])
 embeddings = embed(titles)
 
-nn = NearestNeighbors(n_neighbors=10)
+nn = NearestNeighbors(n_neighbors=50)  # Increase the number of neighbors to 50
 nn.fit(embeddings)
 
-def recommend(movie1, movie2):
+def recommend(movie1_id, movie2_id, selected_languages):
+    movie1_row = df[df['id'] == movie1_id]
+    movie2_row = df[df['id'] == movie2_id]
+
+    if movie1_row.empty or movie2_row.empty:
+        print('Invalid movie id(s).')
+        return
+
+    movie1, movie1_languages = movie1_row.iloc[0]['overview'], movie1_row.iloc[0]['original_language']
+    movie2, movie2_languages = movie2_row.iloc[0]['overview'], movie2_row.iloc[0]['original_language']
+
+    # Filter movies based on selected languages
+    selected_language_movies = df[df['original_language'].isin(selected_languages)]['overview'].tolist()
+    
     emb1 = embed([movie1])
     emb2 = embed([movie2])
     neighbors1 = nn.kneighbors(emb1, return_distance=False)[0]
     neighbors2 = nn.kneighbors(emb2, return_distance=False)[0]
     
     # Calculate recommendations based on popularity as well
-    popularity_recommendations1 = df.nlargest(10, 'popularity')['original_title'].tolist()
-    popularity_recommendations2 = df.nlargest(10, 'popularity')['original_title'].tolist()
+    popularity_recommendations1 = df[df['original_language'].isin(selected_languages)].nlargest(10, 'popularity')['id'].tolist()
+    popularity_recommendations2 = df[df['original_language'].isin(selected_languages)].nlargest(10, 'popularity')['id'].tolist()
     
     # Combine popularity-based recommendations with embeddings-based recommendations
-    combined_recommendations = list(set(popularity_recommendations1 + popularity_recommendations2 + df['original_title'].iloc[neighbors1].tolist() + df['original_title'].iloc[neighbors2].tolist()))
+    combined_recommendations = list(set(popularity_recommendations1 + popularity_recommendations2 + df['id'].iloc[neighbors1].tolist() + df['id'].iloc[neighbors2].tolist()))
     
-    return combined_recommendations
+    return combined_recommendations  # Return a list of recommended movie IDs
 
-movie1 = sys.argv[1]
-movie2 = sys.argv[2]
-combined_recommendations = recommend(movie1, movie2)
+movie1_id = int(sys.argv[1])
+movie2_id = int(sys.argv[2])
+selected_languages = sys.argv[3:]  # Accept the list of selected languages
+combined_recommendations = recommend(movie1_id, movie2_id, selected_languages)
 
 # Print recommendations to be captured by Node.js
 for recommendation in combined_recommendations:
-    print(recommendation)
+    print(recommendation)  # Print the recommended movie IDs
